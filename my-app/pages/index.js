e6f88f4b0eb7320814c5b35c4e614fed29cee91b
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-09-17 17:45:19 +0200

    added gradient to nav and font to devby

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,5 +1,7 @@
 // import { Inter } from 'next/font/google'
+
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from 'react-icons/bs'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -11,15 +13,26 @@ export default function Home() {
           my next js page
         </title>
       </Head>
-      <main>
-        <h1>
-          this is my page
-        </h1>
+      <main className='bg-white px-10'>
         <section className='min-h-screen'>
-          <nav>
-            <h1>
-              dev by carlos
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='font-burtons'>
+              Developed by csarmientobaca
             </h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+              </li>
+
+              <li>
+                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
+              </li>
+
+              <li>
+                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Projects</a>
+              </li>
+            </ul>
+
           </nav>
         </section>
       </main>
